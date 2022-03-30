9104f60ad8059142cbd931bc815edd7a20b2b44c
danylozakharchenko <danylo.zakharcehnko@gmail.com>
2022-03-30 08:36:39 +0200

    single book page and some stylish

--- book/views.py
+++ book/views.py
@@ -8,3 +8,10 @@ class BookListView(generic.ListView):
     model = Book
     context_object_name = "books"
     template_name = 'book/list.html'
+
+
+class BookDetailView(generic.DetailView):
+
+    model = Book
+    context_object_name = "book"
+    template_name = 'book/detail.html'
