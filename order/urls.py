6e89268efdf8aa21ae7a96caef28b58e25da9857
EasternCheesE <easterntwelve@gmail.com>
2022-04-01 16:58:27 +0300

    [order] added order sorting based on book sorting design

--- order/urls.py
+++ order/urls.py
@@ -3,10 +3,5 @@ from order.views import *
 
 urlpatterns = [
     path('all/', OrderListView.as_view(), name="all_orders"),
-    path('sort_id', SortedIDOrderView.as_view(), name="sort_id_orders"),
-    path('sort_user_id', SortedUserIdOrderView.as_view(), name="sort_user_id_orders"),
-    path('sort_book_id', SortedBookIdOrderView.as_view(), name="sort_book_id_orders"),
-    path('sort_created', SortedCreatedOrderView.as_view(), name="sort_created_orders"),
-    path('sort_ended', SortedEndOrderView.as_view(), name="sort_end_orders"),
-    path('sort_plated', SortedPlatedOrderView.as_view(), name="sort_plated_orders"),
+    path('sorted/', SortedOrderView.as_view(), name="sorted_orders"),
 ]
--- order/views.py
+++ order/views.py
@@ -9,57 +9,17 @@ class OrderListView(generic.ListView):
     context_object_name = "orders"
     template_name = 'order/list.html'
 
-class SortedIDOrderView(generic.ListView):
+class SortedOrderView(generic.ListView):
 
+    template_name = 'order/sorted.html'
     context_object_name = 'orders'
-    template_name = 'order/list.html'
 
     def get_queryset(self):
 
-        return Order.objects.order_by('id')
+        param = self.request.GET.get('param', '?')
+        sorting = self.request.GET.get('sorting')
+        queryset = Order.objects.order_by(param)
 
-class SortedUserIdOrderView(generic.ListView):
-
-    context_object_name = 'orders'
-    template_name = 'order/list.html'
-
-    def get_queryset(self):
-
-        return Order.objects.order_by('user_id')
-
-class SortedBookIdOrderView(generic.ListView):
-
-    context_object_name = 'orders'
-    template_name = 'order/list.html'
-
-    def get_queryset(self):
-
-        return Order.objects.order_by('book_id')
-
-
-class SortedCreatedOrderView(generic.ListView):
-
-    context_object_name = 'orders'
-    template_name = 'order/list.html'
-
-    def get_queryset(self):
-
-        return Order.objects.order_by('created_at')
-
-class SortedEndOrderView(generic.ListView):
-
-    context_object_name = 'orders'
-    template_name = 'order/list.html'
-
-    def get_queryset(self):
-
-        return Order.objects.order_by('end_at')
-
-class SortedPlatedOrderView(generic.ListView):
-
-    context_object_name = 'orders'
-    template_name = 'order/list.html'
-
-    def get_queryset(self):
-
-        return Order.objects.order_by('plated_end_at')
+        if sorting == 'asc':
+            return queryset
+        return queryset.reverse()
