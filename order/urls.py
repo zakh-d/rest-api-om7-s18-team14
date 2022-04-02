21707856d7f9d2b029756066e293f77201154c9a
EasternCheesE <easterntwelve@gmail.com>
2022-04-02 19:33:53 +0300

    [order_debtors] added a page and functionality to see the list of users who haven't returned books on time]

--- order/urls.py
+++ order/urls.py
@@ -4,5 +4,6 @@ from order.views import *
 urlpatterns = [
     path('all/', OrderListView.as_view(), name="all_orders"),
     path('sorted/', SortedOrderView.as_view(), name="sorted_orders"),
-    path('user_books/<int:pk>', OrderUserBooksView.as_view(), name="user_books")
+    path('user_books/<int:pk>', OrderUserBooksView.as_view(), name="user_books"),
+    path('debtors/', OrderDebtorsView.as_view(), name="debtors"),
 ]
--- order/views.py
+++ order/views.py
@@ -1,11 +1,7 @@
 from django.views import generic
-
+from django.db.models import F
 from order.models import Order
 from authentication.models import CustomUser
-from book.models import Book
-from django.shortcuts import render
-import logging
-
 
 
 class OrderListView(generic.ListView):
@@ -14,37 +10,23 @@ class OrderListView(generic.ListView):
     context_object_name = "orders"
     template_name = 'order/list.html'
 
-class SortedOrderView(generic.ListView):
 
+class SortedOrderView(generic.ListView):
     template_name = 'order/sorted.html'
     context_object_name = 'orders'
 
     def get_queryset(self):
-
         param = self.request.GET.get('param', '?')
         sorting = self.request.GET.get('sorting')
         queryset = Order.objects.order_by(param)
 
         if sorting == 'asc':
             return queryset
+
         return queryset.reverse()
 
 
-class OrderDebtorsView(generic.ListView):
-
-    model = Order
-    template_name = "order/debtors.html"
-
-    def get_context_data(self, **kwargs):
-        context = super(OrderDebtorsView, self).get_context_data(**kwargs)
-        orders_1 = Order.objects.filter(end_at=None).values_list('user_id', flat=True)
-        context['users'] = CustomUser.objects.all()
-        context['books'] = Order.objects.all()
-        return context
-
-
 class OrderUserBooksView(generic.DetailView):  # Використовуємо DetailView, бо він сам дістає юзера по pk
-
     model = CustomUser
     context_object_name = 'user'
     template_name = 'order/user_books.html'
@@ -54,4 +36,17 @@ class OrderUserBooksView(generic.DetailView):  # Використовуємо De
         user = self.get_object()
         books = list(map(lambda order: order.book, user.orders.all()))
         context['books'] = books
+        
         return context
+
+
+class OrderDebtorsView(generic.ListView):
+    model = Order
+    context_object_name = 'users'
+    template_name = 'order/debtors.html'
+
+    def get_queryset(self):
+        late_returners = Order.objects.filter(end_at__gte=F('plated_end_at')).values_list("user_id", flat=True)
+        debtors = Order.objects.filter(end_at=None).values_list("user_id", flat=True)
+
+        return CustomUser.objects.all().filter(pk__in=late_returners|debtors)
