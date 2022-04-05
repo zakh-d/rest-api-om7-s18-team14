20833de821c7604e10f75338ad0ccc76f678de5c
danylozakharchenko <danylo.zakharcehnko@gmail.com>
2022-04-05 18:04:00 +0200

    [authentication] added form for updating CustomUser

--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,6 @@
 from django.views import generic
 
-from authentication.forms import CustomUserCreationForm
+from authentication.forms import CustomUserCreationForm, CustomUserUpdateForm
 from authentication.models import CustomUser
 
 
@@ -8,5 +8,15 @@ class CustomUserCreationView(generic.CreateView):
 
     model = CustomUser
     form_class = CustomUserCreationForm
-    template_name = 'authentication/create.html'
+    template_name = 'authentication/create_update.html'
     success_url = '/book/all'
+    extra_context = {'title': 'Create User'}
+
+
+class CustomUserUpdateView(generic.UpdateView):
+
+    model = CustomUser
+    form_class = CustomUserUpdateForm
+    template_name = 'authentication/create_update.html'
+    success_url = '/book/all'
+    extra_context = {'title': 'Update User'}
