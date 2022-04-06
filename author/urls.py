d2b65cd0c7896bd09fa5da89db3656f0bb78900b
Anastasiia Stos <anastasiiastos@Anastasiias-MacBook-Pro.local>
2022-04-06 23:45:54 +0200

    Add editing for author

--- author/urls.py
+++ author/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from author.views import AuthorListView, AuthorDetailView
-from . import views
+from author.views import AuthorListView, AuthorDetailView, create_author, edit_author
 
 urlpatterns = [
     path('all/', AuthorListView.as_view(), name="all_authors"),
     path('<int:pk>/', AuthorDetailView.as_view(), name="author_detail"),
-    path('new/', views.create_author, name="create_author"),
+    path('new/', create_author, name="create_author"),
+    path('edit/<int:pk>/', edit_author, name="edit_author"),
 ]
