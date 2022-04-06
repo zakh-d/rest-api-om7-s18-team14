e8aa3e1451226200732e8fd9106d26d9c2058c9b
danylozakharchenko <danylo.zakharcehnko@gmail.com>
2022-04-06 17:33:32 +0200

    [order] added update form for orders

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from authentication.views import CustomUserCreationView, CustomUserUpdateView
 
 urlpatterns = [
-    path('create', CustomUserCreationView.as_view(), name="create_user"),
-    path('update/<int:pk>', CustomUserUpdateView.as_view(), name="update_user"),
+    path('create/', CustomUserCreationView.as_view(), name="create_user"),
+    path('update/<int:pk>/', CustomUserUpdateView.as_view(), name="update_user"),
 ]
